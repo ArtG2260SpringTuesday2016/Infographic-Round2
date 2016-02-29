dbb071dd1f651b776d9d7055292fc81601e47f8e
FlyingPig13 <anotherhale.m@gmail.com>
2016-02-29 03:34:31 -0500

    Start
    Gathered files in one place, linked data to JS file, created list of
    what the data is as well as how it will be used

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//data is an array of objects consisting of 
+//A string "Address"
+//A string "EnergyStar_Certified"
+//A string EnergyStar_Score
+//An integer GHG_Emissions
+//A string GHG_Intensity
+//An integer Gross_Sq_Ft
+//An unknown OnSite_Solar
+//A string Owner_Submitted_Info
+//A string Owner_Submitted_Link
+//A string Percent_Electr
+//A string Percent_Gas
+//A string Percent_Steam
+//A string Property_Name
+//A string Property_Type
+//A string Property_Uses
+//An integer Site_EUI
+//A string Site_Energy_Use
+//A string Tax_Parcel
+//An integer Water_Intensity
+//An integer Year_Built
+//A string ZIP
+
+//For the purposes of this program only 
+//Site_Energy_Use and Year_Built will be used
+//in order to compare amount of energy used to the age of the building
+
+console.log(data);
