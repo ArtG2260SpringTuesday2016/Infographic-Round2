bbec85b26aa72950eaf5fa03238bd18a9c720718
heyryanleys <ryanleys@gmail.com>
2016-03-01 15:36:26 -0500

    Added Line
    - Added line
    - Changed basic formatting
    - Cleared up code

--- sketch.js
+++ sketch.js
@@ -48,88 +48,88 @@ var myData = [
     "date": "filler",
     "interest": 0
   },{
-      "date": "1/4/16",
-      "interest": 0
-    },{
-      "date": "1/5/16",
-      "interest": 0
-    },{
-      "date": "1/6/16",
-      "interest": 0
-    },{
-      "date": "1/7/16",
-      "interest": 0
-    },{
-      "date": "1/8/16",
-      "interest": 1
-    },{
-      "date": "1/9/16",
-      "interest": 1
-    },{
-      "date": "1/10/16",
-      "interest": 0
-    },{
-      "date": "1/11/16",
-      "interest": 100
-    },{
-      "date": "1/12/16",
-      "interest": 41
-    },{
-      "date": "1/13/16",
-      "interest": 14
-    },{
-      "date": "1/14/16",
-      "interest": 10
-    },{
-      "date": "1/15/16",
-      "interest": 7
-    },{
-      "date": "1/16/16",
-      "interest": 6
-    },{
-      "date": "1/17/16",
-      "interest": 6
-    },{
-      "date": "1/18/16",
-      "interest": 4
-    },{
-      "date": "1/19/16",
-      "interest": 3
-    },{
-      "date": "1/20/16",
-      "interest": 2
-    },{
-      "date": "1/21/16",
-      "interest": 2
-    },{
-      "date": "1/22/16",
-      "interest": 2
-    },{
-      "date": "1/23/16",
-      "interest": 2
-    },{
-      "date": "1/24/16",
-      "interest": 2
-    },{
-      "date": "1/25/16",
-      "interest": 1
-    },{
-      "date": "1/26/16",
-      "interest": 1
-    },{
-      "date": "1/27/16",
-      "interest": 1
-    },{
-      "date": "1/28/16",
-      "interest": 1
-    },{
-      "date": "1/29/16",
-      "interest": 1
-    },{
-      "date": "1/30/16",
-      "interest": 2
-    }
-  ];
+    "date": "1/4/16",
+    "interest": 0
+  },{
+    "date": "1/5/16",
+    "interest": 0
+  },{
+    "date": "1/6/16",
+    "interest": 0
+  },{
+    "date": "1/7/16",
+    "interest": 0
+  },{
+    "date": "1/8/16",
+    "interest": 1
+  },{
+    "date": "1/9/16",
+    "interest": 1
+  },{
+    "date": "1/10/16",
+    "interest": 0
+  },{
+    "date": "1/11/16",
+    "interest": 100
+  },{
+    "date": "1/12/16",
+    "interest": 41
+  },{
+    "date": "1/13/16",
+    "interest": 14
+  },{
+    "date": "1/14/16",
+    "interest": 10
+  },{
+    "date": "1/15/16",
+    "interest": 7
+  },{
+    "date": "1/16/16",
+    "interest": 6
+  },{
+    "date": "1/17/16",
+    "interest": 6
+  },{
+    "date": "1/18/16",
+    "interest": 4
+  },{
+    "date": "1/19/16",
+    "interest": 3
+  },{
+    "date": "1/20/16",
+    "interest": 2
+  },{
+    "date": "1/21/16",
+    "interest": 2
+  },{
+    "date": "1/22/16",
+    "interest": 2
+  },{
+    "date": "1/23/16",
+    "interest": 2
+  },{
+    "date": "1/24/16",
+    "interest": 2
+  },{
+    "date": "1/25/16",
+    "interest": 1
+  },{
+    "date": "1/26/16",
+    "interest": 1
+  },{
+    "date": "1/27/16",
+    "interest": 1
+  },{
+    "date": "1/28/16",
+    "interest": 1
+  },{
+    "date": "1/29/16",
+    "interest": 1
+  },{
+    "date": "1/30/16",
+    "interest": 2
+  }
+];
 
 var graph = function (someData) {
   for (var i = 0; i < someData.length; i++){
@@ -146,14 +146,21 @@ function setup() {
   createCanvas(1000, 500)
   background(0)
   noStroke()
+// Adding color to histogram
   fill(34,148,143)
   graph(myData);
+
+//Adding text to corner
   textSize(20)
   text("David Bowie in Google Searches", 617, 70),
   fill(34,148,143),
   rect(590,80,350,30),
   fill(255),
   text("From January 4 to January 30 2016", 603, 102)
+
+// Adding Line at 0
+  stroke(300);
+  line(200, 250, 800, 250);
 }
 
 function draw() {
