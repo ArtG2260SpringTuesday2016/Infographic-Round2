42482c47548772f461155237bba178be72646b4e
AnthonyBrandao <Brandao.a@husky.neu.edu>
2016-03-01 18:09:16 -0500

    attempt3
    got the colors working

--- sketch.js
+++ sketch.js
@@ -9,16 +9,24 @@ function setup() {
 function draw() {
   background(100);
   pieChart(300, angles);
+  pieChart2(300, angles);
 }
 
 function pieChart(diameter, data) {
   var lastAngle = 0;
   for (var i = 0; i < data.length; i++) {
-    var bluee = map(i, 0, data.length, 0, 255);
-    fill(bluee);
-    arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+    fill(random (0, 255), 0, 0);
+    arc(width/1.35, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
     lastAngle += radians(angles[i]);
   }
-  
+}
+
+function pieChart2(diameter, data) {
+  var lastAngle = 0;
+  for (var i = 0; i < data.length; i++) {
+    fill (0, 0, random (0, 255));
+    arc(width/4, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+    lastAngle += radians(angles[i]);
+  }  
   
 }
