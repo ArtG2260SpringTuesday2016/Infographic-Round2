c1a82d5d635b2706c7f295dd61042fa0b8c6f24a
sukanye <ray.suk@husky.neu.edu>
2016-03-01 20:23:01 -0500

    Some Changes
    @FranDias

--- sketch.js
+++ sketch.js
@@ -13,12 +13,6 @@ function draw() {
   
   function kanye() {
   
-  /*
-  var discography = [
-  {album: "The College Dropout", song: "Intro", albumLoc: [30,500], lyrics: ""},
-  {album: "The College Dropout", song: "We Don't Care", albumLoc: [100,500], lyrics: "Oh yeah, I've got the perfect song for the kids to sing And all my people that's... Drug dealin' just to get by Stack ya' money 'til it get sky high (Kids, sing! Kids, sing!) We wasn't s'posed to make it past 25 Joke's on you, we still alive Throw your hands up in the sky and say: We don't care what people say If this is your first time hearin' this You are about to experience somethin' so cold, man We never had nothin' handed, took nothin' for granted Took nothin' from no man, man, I'm my own man But as a shorty I looked up to the dopeman Only adult man I knew that wasn't broke, man Flickin' Starter coats, man Man, you don't know, man We don't care what people say This is for my niggas outside all winter Cause this summer they ain't finna say: next summer I'm finna Sittin' in the hood like community colleges This dope money here is Lil' Trey's scholarship Cause ain't no tuition for having no ambition And ain't no loans for sittin' your ass at home So we forced to sell crack, rap, and get a job You gotta do somethin' man, your ass is grown The second verse is for my dogs workin' 9 to 5 that still hustle Cause a nigga can't shine off $6.55 And everybody sellin' makeup, Jacob's And bootlegged tapes just to get they cake up We put shit on layaway, then come back We claim other people kids on our income tax We take that money, cop work, then push packs to get paid And we don't care what people say Momma say she wanna move South Scratchin' lottery tickets, eyes on a new house 'Round the same time, Doe ran up in dude house Couldn't get a job, so since he couldn't get work He figured he'd take work The drug game bulimic, it's hard to get weight A nigga's money is homo, it's hard to get straight But we gon' keep bakin' til the day we get cake And we don't care what people say You know the kids gon' act a fool When you stop the programs for after school And they DCFS, some of 'em dyslexic They favorite 50 Cent song 12 Questions We scream: rocks, blow, weed, park, see, now we smart We ain't retards, the way teachers thought Hold up, hold fast, we make more cash Now tell my momma I belong in that slow class Sad enough we on welfare They tryna put me on the school bus with the space for the wheelchair I'm tryna get the car with the chromie wheels here They tryna cut our lights out like we don't live here Look what was handed us, fathers abandoned us When we get them hammers, go on, call the ambulance Sometimes I feel no one in this world understands us But we don't care what people say My niggas"},
-  ];*/
-  
   var discography = [
     {
       "name": "The College Dropout",
@@ -26,108 +20,108 @@ function draw() {
         {
           name: "Intro",
           albumLoc: [30, 500],
-          lyrics: ""
-        }
+          lyrics: "" 
+        },
         {
           name: "We Don't Care",
           albumLoc: [80, 500],
-          lyrics: "Oh yeah, I've got the perfect song for the kids to sing And all my people that's... Drug dealin' just to get by Stack ya' money 'til it get sky high (Kids, sing! Kids, sing!) We wasn't s'posed to make it past 25 Joke's on you, we still alive Throw your hands up in the sky and say: We don't care what people say If this is your first time hearin' this You are about to experience somethin' so cold, man We never had nothin' handed, took nothin' for granted Took nothin' from no man, man, I'm my own man But as a shorty I looked up to the dopeman Only adult man I knew that wasn't broke, man Flickin' Starter coats, man Man, you don't know, man We don't care what people say This is for my niggas outside all winter Cause this summer they ain't finna say: next summer I'm finna Sittin' in the hood like community colleges This dope money here is Lil' Trey's scholarship Cause ain't no tuition for having no ambition And ain't no loans for sittin' your ass at home So we forced to sell crack, rap, and get a job You gotta do somethin' man, your ass is grown The second verse is for my dogs workin' 9 to 5 that still hustle Cause a nigga can't shine off $6.55 And everybody sellin' makeup, Jacob's And bootlegged tapes just to get they cake up We put shit on layaway, then come back We claim other people kids on our income tax We take that money, cop work, then push packs to get paid And we don't care what people say Momma say she wanna move South Scratchin' lottery tickets, eyes on a new house 'Round the same time, Doe ran up in dude house Couldn't get a job, so since he couldn't get work He figured he'd take work The drug game bulimic, it's hard to get weight A nigga's money is homo, it's hard to get straight But we gon' keep bakin' til the day we get cake And we don't care what people say You know the kids gon' act a fool When you stop the programs for after school And they DCFS, some of 'em dyslexic They favorite 50 Cent song 12 Questions We scream: rocks, blow, weed, park, see, now we smart We ain't retards, the way teachers thought Hold up, hold fast, we make more cash Now tell my momma I belong in that slow class Sad enough we on welfare They tryna put me on the school bus with the space for the wheelchair I'm tryna get the car with the chromie wheels here They tryna cut our lights out like we don't live here Look what was handed us, fathers abandoned us When we get them hammers, go on, call the ambulance Sometimes I feel no one in this world understands us But we don't care what people say My niggas";
-        }
+          lyrics: "Oh yeah, I've got the perfect song for the kids to sing And all my people that's... Drug dealin' just to get by Stack ya' money 'til it get sky high (Kids, sing! Kids, sing!) We wasn't s'posed to make it past 25 Joke's on you, we still alive Throw your hands up in the sky and say: We don't care what people say If this is your first time hearin' this You are about to experience somethin' so cold, man We never had nothin' handed, took nothin' for granted Took nothin' from no man, man, I'm my own man But as a shorty I looked up to the dopeman Only adult man I knew that wasn't broke, man Flickin' Starter coats, man Man, you don't know, man We don't care what people say This is for my niggas outside all winter Cause this summer they ain't finna say: next summer I'm finna Sittin' in the hood like community colleges This dope money here is Lil' Trey's scholarship Cause ain't no tuition for having no ambition And ain't no loans for sittin' your ass at home So we forced to sell crack, rap, and get a job You gotta do somethin' man, your ass is grown The second verse is for my dogs workin' 9 to 5 that still hustle Cause a nigga can't shine off $6.55 And everybody sellin' makeup, Jacob's And bootlegged tapes just to get they cake up We put shit on layaway, then come back We claim other people kids on our income tax We take that money, cop work, then push packs to get paid And we don't care what people say Momma say she wanna move South Scratchin' lottery tickets, eyes on a new house 'Round the same time, Doe ran up in dude house Couldn't get a job, so since he couldn't get work He figured he'd take work The drug game bulimic, it's hard to get weight A nigga's money is homo, it's hard to get straight But we gon' keep bakin' til the day we get cake And we don't care what people say You know the kids gon' act a fool When you stop the programs for after school And they DCFS, some of 'em dyslexic They favorite 50 Cent song 12 Questions We scream: rocks, blow, weed, park, see, now we smart We ain't retards, the way teachers thought Hold up, hold fast, we make more cash Now tell my momma I belong in that slow class Sad enough we on welfare They tryna put me on the school bus with the space for the wheelchair I'm tryna get the car with the chromie wheels here They tryna cut our lights out like we don't live here Look what was handed us, fathers abandoned us When we get them hammers, go on, call the ambulance Sometimes I feel no one in this world understands us But we don't care what people say My niggas"
+        },
         {
           name: "Graduation Day",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "All Falls Down",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "I'll Fly Away",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Spaceship",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Jesus Walks",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Never Let Me Down",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Get Em High",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Workout Plan",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "The New Workout Plan",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Slow Jamz",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Breathe In Breathe Out",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "School Spirit (skit 1)",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "School Spirit",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "School Spirit (skit 2)",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Lil Jimmy",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Two Words",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Through the Wire",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Family Business",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
         {
           name: "Last Call",
           albumLoc: [80, 500],
           lyrics: ""
-        }
+        },
       ]
     },{
       "name": "Late Registration",
@@ -144,7 +138,7 @@ function draw() {
   for(var i = 0; i < discography.length; i++) {
     fill('hotpink');
     textSize(15);
-    text(discography[i].album, discography[i].albumLoc[0], discography[i].albumLoc[1], 600, 600);
+   // text(discography[i].album, discography[i].albumLoc[0], discography[i].albumLoc[1],      600, 600);
     
     //textSize(10);
     //text(discography[i].song, discography[i].albumLoc[0] + 50, discography[i].albumLoc[1], 600, 600);
@@ -154,7 +148,7 @@ function draw() {
       var rectWidth = 30;
       noStroke();
       fill('hotpink');
-      rect((discography[i].albumLoc[0] + 30),(discography[i].albumLoc[1] - 50),rectWidth,rectHeight);
+     // rect((discography[i].albumLoc[0] + 30),(discography[i].albumLoc[1] - 50),rectWidth,rectHeight);
       
     } 
   } 
