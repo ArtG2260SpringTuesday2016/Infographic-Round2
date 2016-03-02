098aae8016de5bd41a0f557d6fe8a8e1724ef872
heyryanleys <ryanleys@gmail.com>
2016-03-01 20:47:31 -0500

    Separated Data
    - Separated Data
    - Simplified Moon Info

--- sketch.js
+++ sketch.js
@@ -1,137 +1,11 @@
+var myData = data
 var stars = [];
-
-var myData = [
-  {
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "filler",
-    "interest": 0
-  },{
-    "date": "1/4/16",
-    "interest": 0
-  },{
-    "date": "1/5/16",
-    "interest": 0
-  },{
-    "date": "1/6/16",
-    "interest": 0
-  },{
-    "date": "1/7/16",
-    "interest": 0
-  },{
-    "date": "1/8/16",
-    "interest": 1
-  },{
-    "date": "1/9/16",
-    "interest": 1
-  },{
-    "date": "1/10/16",
-    "interest": 0
-  },{
-    "date": "1/11/16",
-    "interest": 100
-  },{
-    "date": "1/12/16",
-    "interest": 41
-  },{
-    "date": "1/13/16",
-    "interest": 14
-  },{
-    "date": "1/14/16",
-    "interest": 10
-  },{
-    "date": "1/15/16",
-    "interest": 7
-  },{
-    "date": "1/16/16",
-    "interest": 6
-  },{
-    "date": "1/17/16",
-    "interest": 6
-  },{
-    "date": "1/18/16",
-    "interest": 4
-  },{
-    "date": "1/19/16",
-    "interest": 3
-  },{
-    "date": "1/20/16",
-    "interest": 2
-  },{
-    "date": "1/21/16",
-    "interest": 2
-  },{
-    "date": "1/22/16",
-    "interest": 2
-  },{
-    "date": "1/23/16",
-    "interest": 2
-  },{
-    "date": "1/24/16",
-    "interest": 2
-  },{
-    "date": "1/25/16",
-    "interest": 1
-  },{
-    "date": "1/26/16",
-    "interest": 1
-  },{
-    "date": "1/27/16",
-    "interest": 1
-  },{
-    "date": "1/28/16",
-    "interest": 1
-  },{
-    "date": "1/29/16",
-    "interest": 1
-  },{
-    "date": "1/30/16",
-    "interest": 2
-  }
-];
+var moon = {
+  name: "Moon",
+  size: 100,
+  x: 200,
+  y: 100
+};
 
 var graph = function (someData) {
   for (var i = 0; i < someData.length; i++){
@@ -149,22 +23,20 @@ function setup() {
   background(0)
 
 // Drawing Random Stars
-    for (var i = 0; i < 100; i++){
-      stars[i] = new Star();
-    }
-    for (var i = 0; i < stars.length; i++){
-      stars[i].move();
-      stars[i].display();
-    }
-
-    function Star(){
-        this.x = random(0, width);
-        this.y = random(0, height);
-
-        this.display = function (){
-          noStroke()
-          fill(255)
-          ellipse(this.x, this.y, 5, 5);
+  for (var i = 0; i < 100; i++){
+    stars[i] = new Star();
+  }
+  for (var i = 0; i < stars.length; i++){
+    stars[i].move();
+    stars[i].display();
+  }
+  function Star(){
+  this.x = random(0, width);
+  this.y = random(0, height);
+    this.display = function (){
+      noStroke()
+      fill(255)
+      ellipse(this.x, this.y, 5, 5);
       }
       this.move = function(){
         this.x = this.x + random (0, 0);
@@ -172,35 +44,29 @@ function setup() {
       }
     }
 
-// Adding Rocketship
-noStroke()
-fill(255)
-ellipse(200, 100, 100, 100)
-fill(220)
-ellipse(220, 80, 30, 30)
-ellipse(180, 70, 22, 22)
-ellipse(180, 100, 15, 15)
+// Adding Moon
+  noStroke()
+  fill(255)
+  ellipse(moon.x, moon.y, moon.size, moon.size) // 200,100,100,100
+  fill(220)
+  ellipse(moon.x + 20, moon.y - 20, moon.size - 70, moon.size - 70)
+  ellipse(moon.x - 20, moon.y - 30, moon.size - 78, moon.size - 78)
+  ellipse(moon.x - 20, moon.y, moon.size - 85, moon.size - 85)
 // Creating Histogram
   noStroke()
   fill(34,148,143)
   graph(myData);
 
-//Adding text to corner
+//Adding Text to Corner
   textSize(20)
   text("David Bowie in Google Searches", 617, 70),
   fill(34,148,143),
-  rect(590,80,350,30),
+  rect(590,80,350,30), // Rectangle Behind Text
   fill(255),
   text("From January 4 to January 30 2016", 603, 102)
 
 // Adding Line at 0
   stroke(300);
   line(200, 250, 800, 250);
-
-}
-
-function draw() {
-
-
 
 }
