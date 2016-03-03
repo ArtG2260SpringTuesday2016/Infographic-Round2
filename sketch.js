eb8b070ef7c03e6dce273717b94d4f11e5904550
Jacqueline-G <garruto.j@husky.neu.edu>
2016-03-03 13:47:21 -0500

    tinkered with drawing
    wondering how to deal with the length of the numbers- what is the best
    way to work that

--- sketch.js
+++ sketch.js
@@ -1,299 +1,47 @@
+
 // Snowfall in inches in Boston, MA on each day in January over the included years (2010-2016)
-var snowInJanuary = [
-  {
-  //  "year" : 2016,
-    "1" : 0.000,
-    "2" : 0.000,
-    "3" : 0.000,
-    "4" : 0.000,
-    "5" : 0.000,
-    "6" : 0.000,
-    "7" : 0.000,
-    "8" : 0.000,
-    "9" : 0.000,
-    "10" : 0.000,
-    "11" : 0.000,
-    "12" : 0.300,
-    "13" : 0.000,
-    "14" : 0.000,
-    "15" : 0.000,
-    "16" : 0.000,
-    "17" : 1.300,
-    "18" : 1.800,
-    "19" : 0.000,
-    "20" : 0.000,
-    "21" : 0.000,
-    "22" : 0.000,
-    "23" : 6.100,
-    "24" : 0.000,
-    "25" : 0.000,
-    "26" : 0.000,
-    "27" : 0.000,
-    "28" : 0.000,
-    "29" : 0.000,
-    "30" : 0.000,
-    "31": 0.000
-  },
-    {
-   // "year" : 2015,
-    "1" : 0.000,
-    "2" : 0.000,
-    "3" : 1.400,
-    "4" : 0.000,
-    "5" : 0.000,
-    "6" : 0.200,
-    "7" : 0.000,
-    "8" : 0.000,
-    "9" : 0.200,
-    "10" : 0.000,
-    "11" : 0.000,
-    "12" : 0.000,
-    "13" : 0.000,
-    "14" : 0.000,
-    "15" : 0.800,
-    "16" : 0.000,
-    "17" : 0.000,
-    "18" : 0.000,
-    "19" : 0.000,
-    "20" : 0.000,
-    "21" : 0.000,
-    "22" : 0.000,
-    "23" : 0.000,
-    "24" : 5.100,
-    "25" : 0.000,
-    "26" : 2.300,     
-    "27" : 22.100,
-    "28" : 0.200,
-    "29" : 0.000,
-    "30" : 0.700,
-    "31" : 1.300
-  },
-    {
-   // "year" : 2014,
-      "1" : 0.000,
-      "2" : 10.600,
-      "3" : 4.500,
-      "4" : 0.000,
-      "5" : 0.000,
-      "6" : 0.000,
-      "7" : 0.000,  
-      "8" : -9999.000,  
-      "9" : -9999.000,
-      "10" : 0.200,
-      "11" : 0.000,
-      "12" : 0.000,
-      "13" : 0.000,
-      "14" : 0.000,
-      "15" : 0.000,
-      "16" : 0.000,
-      "17" : 0.000,
-      "18" : 2.000,
-      "19" : 0.200,
-      "20" : 0.000,
-      "21" : 3.900,  
-      "22" : -9999.000,
-      "23" : 0.000,
-      "24" : 0.000,  
-      "25" : -9999.000,
-      "26" : 0.000,
-      "27" : 0.000,
-      "28" : 0.000,
-      "29" : 0.000,
-      "30" : 0.000,
-      "31" : 0.000    
-  },
-    {
-  //  "year" : 2013,
-    "1" : 0.000,  
-    "2" : -9999.000,
-    "3" : 0.000,
-    "4" : 0.000,
-    "5" : 0.000,
-    "6" : 0.000,
-    "7" : 0.000,
-    "8" : 0.000,
-    "9" : 0.000,
-    "10" : 0.000,
-    "11" : 0.000,
-    "12" : 0.000,
-    "13" : 0.000,
-    "14" : 0.000,  
-    "15" : -9999.000,
-    "16" : 3.200,
-    "17" : 0.000,
-    "18" : 0.000,
-    "19" : 0.000,
-    "20" : 0.000,
-    "21" : 0.400, 
-    "22" : -9999.000,
-    "23" : 0.000,
-    "24" : 0.000,
-    "25" : 0.100,  
-    "26" : -9999.000,
-    "27" : 0.000,
-    "28" : 1.000,
-    "29" : 0.100,
-    "30" : 0.000,
-    "31" : 0.000
-    
-  },
-    {
-  //  "year" : 2012,
-    "1" : 0.000,
-    "2" : 0.000,
-    "3" : 0.000,
-    "4" : 0.000,
-    "5" : 0.000,
-    "6" : 0.000,
-    "7" : 0.000,
-    "8" : 0.000,
-    "9" : 0.000,
-    "10" : 0.500,
-    "11" : 0.000,
-    "12" : 0.000,
-    "13" : 0.000,
-    "14" : 0.000,
-    "15" : 0.000,
-    "16" : 1.200,
-    "17" : 0.200,
-    "18" : -9999.000,
-    "19" : -9999.000,
-    "20" : 1.500,
-    "21" : 2.900,
-    "22" : 0.000,
-    "23" : 0.000,
-    "24" : 0.000,
-    "25" : 0.000,
-    "26" : 0.200,
-    "27" : 0.000,
-    "28" : 0.000,
-    "29" : 0.000,
-    "30" : 0.000,
-    "31" : 0.000
-  },
-    {
-   // "year" : 2011,
-    "1" : 0.000,
-    "2" : 0.000,
-    "3" : 0.000,
-    "4" : 0.000,
-    "5" : 0.000,
-    "6" : 0.000,
-    "7" : 0.300,
-    "8" : 1.900,
-    "9" : 1.600,
-    "10" : 0.000,
-    "11" : 0.000,     
-    "12" : 14.600,
-    "13" : 0.000,
-    "14" : 0.000,
-    "15" : 0.000,
-    "16" : 0.000,
-    "17" : 0.000,
-    "18" : 1.900,
-    "19" : 0.000,
-    "20" : 0.000,
-    "21" : 7.300,
-    "22" : 0.000,
-    "23" : 0.000,
-    "24" : 0.000,
-    "25" : 0.800,
-    "26" : 1.100,
-    "27" : 8.800,
-    "28" : 0.000,
-    "29" : 0.000,
-    "30" : 0.000,
-    "31" : 0.000    
-  },
-  {
-    //"year" : 2010,
-    "1" : 0.000,
-    "2" : 6.400,
-    "3" : 3.700,
-    "4" : 0.000,
-    "5" : 0.000,
-    "6" : 0.000,
-    "7" : 0.000,
-    "8" : 0.100,
-    "9" : 0.000,
-    "10" : 0.000,
-    "11" : 0.000,
-    "12" : -9999.000,
-    "13" : 0.000,
-    "14" : 0.000,
-    "15" : 0.000,
-    "16" : 0.000,  
-    "17" : -9999.000,
-    "18" : 1.900,
-    "19" : 0.800,
-    "20" : 0.000,
-    "21" : 0.000,
-    "22" : 0.000,
-    "23" : 0.000,
-    "24" : 0.000,
-    "25" : 0.000,
-    "26" : 0.000,
-    "27" : 0.000,
-    "28" : 0.200,
-    "29" : 0.000,
-    "30" : 0.000,
-    "31" : 0.000   
-  }
-];
+function setup(){
+  createCanvas(1500, 1000)
+  
+  // snowInjan
+  drawRectangleHelpers();
+}
+//var snowInJanuary = snowInjan
 
 
-function calculateAvg(snowInJanuary) {
+function calculateAvg(snowInjan){
   
   var avgSnowfall = {};
   
   // array for loop
-  for (var i = 0; i < snowInJanuary.length; i++) {
-    
-    // represents a dictionary for the current year 
-    var currentYear = snowInJanuary[i];
-  }
+  for (var i = 0; i < snowInjan.length; i++) {
+   // represents a dictionary for the current year 
+    var currentYear = snowInjan[i];
     // dictionary for loops
     for (var key in currentYear) {
-      // key == "year", currentYear[key] == 2016
-      
+      // key == "1" or "2" or "3", currentYear[key] == 0.006   (snowfall # in inches)
       // check if key already exists in dictionary 
       // -- if yes then update the value to be the new avg
       // if no then add the new key and value
       if (key in avgSnowfall) {
         avgSnowfall[key].push(currentYear[key]);
+        
+        // dict = {"1" : [6, 7, 8, 9, 10],
+        //          "2" : [.006, .7, .4]}
       }
       else {
+        // dict = {"1" : [6]}
         avgSnowfall[key] = [ currentYear[key] ];
+        }
       }
-      
     }
-    
-  }
-  
-  // update dictionary arrays to be avgs and not arrays
-  // (go through each element in the array and add it together. Then divide this total by the number of elements in the array to get the average. Then overwrite the value in the dictionary to be this new avg instead of an array of numbers.)
-  
-  /*
-  
-  example
-  avgSnowfall = {
-  "1" : [1,2,3,4,5],   ->    "1" : 3,
-  "2" : [5.66, 0.000, 4.6000, -9.9999]
-  }
-   
-  */
   
   // go through our new dictionary
   for(var key in avgSnowfall) {
-    
     var valuesInArray = avgSnowfall[key];
-    
   //  console.log('key: ' + key + ' -- ' + valuesInArray);
-    
     var divisor = valuesInArray.length;
-    
     var dividend = 0;
-    
     // go through the array and sum up each element
     for (var i = 0; i < valuesInArray.length; i++) {
       if(valuesInArray[i] != -9999.000) {
@@ -307,31 +55,36 @@ function calculateAvg(snowInJanuary) {
     // update the value in the key value pair of the dictionary
     avgSnowfall[key] = avg;
   }
-  
   return avgSnowfall;
-  
 }
 
-
-//console.log(0.000 + 0.000);
-
-var actualData= calculateAvg(snowInJanuary);
-
-for (var key in actualData) {
   
-  var day = key;
-  var avgValue = actualData[key];
   
-  drawRectangle(day, avgValue);
+function drawRectangleHelpers() {
+
+  var avgSnowfall= calculateAvg(snowInJanuary);
+  //   {
+  //    "1" : .0056,
+  //    "2" : .763, ...
+  //  }
   
+  for (var key in avgSnowfall) {
+    var day = key;
+    var avgValue = avgSnowfall[key];
+    drawRectangle(day, avgValue);
+  }
+
 }
-
-
+  
 function drawRectangle(day, value) {
-  rect(day * 25, 0, 10, value * 100)
+  var yCoord = 950-(value*100);
+  var xCoord = (day * 25) + 250;
+  
+  rect(xCoord, yCoord, 15, value*100);
+  text(day, xCoord, 980);
+  text(value, xCoord, yCoord + 10);
 }
 
 
-console.log(calculateAvg(snowInJanuary));
 
 
