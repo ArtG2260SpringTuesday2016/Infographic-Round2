50f29ef09d7d89b051ef5917bd0fb4ad4494ec92
AlexisDiefenbach <aodiefenbach@gmail.com>
2016-03-21 23:18:19 -0400

    Attempt 4
    cant get the hover statement to work breaks code.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,31 @@
 var angles = [ 80, 20, 35, 45, 50, 50, 30, 50 ];
 
+function showStat(){
+ d = text('All Blue colors', 150, 160, 500, 500);
+}
+
+function showStat2(){
+ c = text('All Red colors', 500, 160, 500, 500);
+}
+
 function setup() {
   createCanvas(720, 400);
   noStroke();
   noLoop();  
+  pie1 = pieChart;
+  pie2 = pieChart2;
+  pie1.mouseOver(showStat);
+  pie2.mouseOver(showStat2);
+  
 }
 
 function draw() {
   background(100);
   pieChart(300, angles);
   pieChart2(300, angles);
+  text('Blue Shades', 150, 360, 500, 500);
+  text('Red Shades', 500, 360, 500, 500);
+  ellipse(width/2, height/2, d, d);
 }
 
 function pieChart(diameter, data) {
@@ -28,5 +44,6 @@ function pieChart2(diameter, data) {
     arc(width/4, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
     lastAngle += radians(angles[i]);
   }  
-  
+
+
 }
