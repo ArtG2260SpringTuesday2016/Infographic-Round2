cb6ed013b0d100f63561ee9f1092d1b4d584898b
AnthonyBrandao <Brandao.a@husky.neu.edu>
2016-03-22 00:21:08 -0400

    new temporary additions

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ for (var i = 0; i < hitters.length; i++) {
   hitters[i] = hitters[i];
 }
 
+var isOverCircle;
 
 function setup() {
   createCanvas(800,800)
@@ -17,19 +18,64 @@ function setup() {
   for (var i = 0; i < homeRun.length; i++){
     var ellipseHeight = homeRun[i]*PI ;
     var ellipseWidth =  700;
-
+ var distance = dist(mouseX, mouseY, ellipseHeight, ellipseHeight); 
+  
+  if(distance < 100)
+  {
+    isOverCircle = true;
+  } else {
+    isOverCircle = false;
+  }
+  
     fill (random(1,255), random (1,255), random(1,255));
     noStroke()
     text(homeRun[i], [i*70], (ellipseWidth+5));
+    if(isOverCircle == true)
+  {
+    fill(1);
+    cursor(HAND);
+    text ('I WORK!!!', 250, 250)
+  } 
+  else {
+    fill(200); 
+    cursor(ARROW);
+  
+  }
     ellipse([i*80] , (ellipseWidth - ellipseHeight), ellipseHeight, ellipseHeight);
     
   }
   
   
   for (var i = 0; i < hitters.length; i++){
-    
-    text(hitters[i], [i*70], (ellipseWidth-200));
+    fill (random(1,255), random (1,255), random(1,255));
+    text(hitters[i], [i*170], (ellipseWidth-200));
   
   }
 }
 
+function draw(){
+  
+  var distance = dist(mouseX, mouseY, 200, 200); 
+  
+  if(distance < 50)
+  {
+    isOverCircle = true;
+  } else {
+    isOverCircle = false;
+  }
+  
+   if(isOverCircle == true)
+  {
+    fill(100);
+    cursor(HAND);
+    text ('I WORK!!!', 250, 250)
+  } else {
+    fill(200); 
+    cursor(ARROW);
+  
+  }
+   ellipse(200, 200, 100, 100);
+  
+ 
+  
+}
