5f9aa3bea78972b2bbeb3f9fb1ac961047e21008
Francisco Dias <fdias@hubspot.com>
2016-03-24 18:36:12 -0400

    find frequency of words in a song

--- sketch.js
+++ sketch.js
@@ -3,6 +3,30 @@
 var meWords = ["I","me","Kanye","West"]
 var weWords = ["you","we","youre","they", "he","she","them"]
 
+// Splits a string into an array of words
+var splitWords = function(aString) {
+  return aString.split(" ");
+}
+
+// Takes in an array of words and gives frequency
+var tallyWordsUp = function(anArrayOfWords) {
+  var wordObject = {};
+
+  // Initalize values in object
+  for (word in anArrayOfWords) {
+    wordObject[anArrayOfWords[word]] = 0
+  }
+
+  // Tally up occurences
+  for (word in anArrayOfWords) {
+    wordObject[anArrayOfWords[word]] += 1
+  }
+  return wordObject;
+}
+
+
+
+
 function setup() {
   createCanvas(1120,600);
   background(51);
