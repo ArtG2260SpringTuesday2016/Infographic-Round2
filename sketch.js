5f7effb2e6062b9504d3b6dd9f59c8910f15922c
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-04-05 17:22:36 -0400

    added text
    added descriptive text on hover over points

--- sketch.js
+++ sketch.js
@@ -28,9 +28,28 @@ function setup() {
   for (var i = 0; i < cityData.length; i++){
     makesDataPoint(cityData[i])
   }
+  
+  
 
 
   textSize(20)
   text("City Population", 400,575)
   text("Crime Rate", 20,250,20,100)
+}
+
+var dataPointText = "Hover over points for more info"
+
+function draw(){
+  var hit=false;
+  for (var i = 0; i < cityData.length; i++){
+   hit = collidePointCircle(mouseX,mouseY,(cityData[i].Population/3000)+50,cityData[i].ViolentCrime/4,7,7)
+  
+  if (hit){
+    dataPointText =
+    "Name: " + cityData[i].City + "\n" + 
+    "Population: " + cityData[i].Population + "\n" +
+    "Violent Crime Rate: " + cityData[i].ViolentCrime
+  }
+  }
+  text(dataPointText,500,400)
 }
