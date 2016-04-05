c7909317b716bb3119c2f334c85dc84926df8f85
AlexisDiefenbach <aodiefenbach@gmail.com>
2016-04-04 22:09:12 -0400

    Added mousehover with some problems
    I wasnt able to get hover to work before because i had no loop on but
    now that it is off the pie charts are flickering. If I take off the
    random color it will go back to one solid color for the pie charts.

--- sketch.js
+++ sketch.js
@@ -1,37 +1,57 @@
 var angles = [ 80, 20, 35, 45, 50, 50, 30, 50 ];
 
-function showStat(){
- d = text('All Blue colors', 150, 160, 500, 500);
-}
-
-function showStat2(){
- c = text('All Red colors', 500, 160, 500, 500);
-}
 
 function setup() {
   createCanvas(720, 400);
-  noStroke();
-  noLoop();  
-  pie1 = pieChart;
-  pie2 = pieChart2;
-  pie1.mouseOver(showStat);
-  pie2.mouseOver(showStat2);
-  
 }
 
 function draw() {
-  background(100);
+  
+  var mouseloc = dist(mouseX, mouseY, 182, 200); 
+  var mouseloc2 = dist(mouseX, mouseY, 532, 200); 
+  
+  if(mouseloc < 150)
+  {
+    hovercircle = true;
+  }else
+  {
+    hovercircle = false;
+  }
+  
+   if(hovercircle == true)
+  {
+    fill(100);
+    text ('Blue 15% Azure 10% Teal 30% Cyan 20% Denim 13% Skyblue 10%', 10, 40)
+  }else 
+  {
+  }
+
+  if(mouseloc2 < 150)
+  {
+    hovercircle = true;
+  }else
+  {
+    hovercircle = false;
+  }
+  
+  if(hovercircle == true)
+  {
+    fill(100);
+    cursor(HAND);
+    text ('red 10% pink 19% lava 30% Rose 10% Flame 13% Crimson 20%', 400, 40)
+  }else
+  {
+  }
   pieChart(300, angles);
   pieChart2(300, angles);
   text('Blue Shades', 150, 360, 500, 500);
   text('Red Shades', 500, 360, 500, 500);
-  ellipse(width/2, height/2, d, d);
 }
 
 function pieChart(diameter, data) {
   var lastAngle = 0;
   for (var i = 0; i < data.length; i++) {
-    fill(random (0, 255), 0, 0);
+    fill(random (0, 250), 0, 0);
     arc(width/1.35, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
     lastAngle += radians(angles[i]);
   }
@@ -39,11 +59,11 @@ function pieChart(diameter, data) {
 
 function pieChart2(diameter, data) {
   var lastAngle = 0;
+
   for (var i = 0; i < data.length; i++) {
-    fill (0, 0, random (0, 255));
+    fill (0, 0, random (0, 250));
     arc(width/4, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
     lastAngle += radians(angles[i]);
   }  
-
 
 }
