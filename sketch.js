dde63c06083c59d7e8e436e23e172820f538e9d4
Francisco Dias <fdias@hubspot.com>
2016-04-05 19:58:34 -0400

    words show up now

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
 var data = [4062, 1735, 1474, 1880, 13461, 11230, 12974, 10683, 47062, 38221, 48112,  42521, 55342,  26617,  62236, 29665];
 
+var words = {}
+words["text"] =  "";
+words["x"] = 0;
+words["y"] = 0;
+
 function setup(){
 createCanvas(1000,615);
 }
 
 function draw(){
+background(255)
 
 for (var i=0; i <= 16; i++){
 
@@ -12,64 +18,80 @@ for (var i=0; i <= 16; i++){
   var yPosition = 550;
   var rectWidth = 5;
   var datayear= -data[i]/120;
-  
+
   var check = collidePointCircle(mouseX,mouseY,153, 470, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
-		text("90 Million",130,450);
+		words["text"] = "90 Million";
+    words["x"] = 130;
+    words["y"] = 450;
 		noStroke;
 	}
-	
+
 	var check = collidePointCircle(mouseX,mouseY, 176, 460, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
 	  noStroke;
-		text("100 Million",150,450);
+		words["text"] = "100 Million";
+    words["x"] = 150;
+    words["y"] = 450;
 	}
-		
+
 	var check = collidePointCircle(mouseX,mouseY, 268, 430, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
 	  noStroke;
-		text("130 Million",245,415);
+		words["text"] = "130 Million";
+    words["x"] = 245;
+    words["y"] = 415;
 	}
-	 
-	
+
+
 	var check = collidePointCircle(mouseX,mouseY, 337, 410, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
 	  noStroke;
-		text("150 Million",310,400);
+		words["text"] = "150 Million";
+    words["x"] = 310;
+    words["y"] = 400;
 	}
-	
+
 	var check = collidePointCircle(mouseX,mouseY, 517, 360, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
 	  noStroke;
-		text("200 Million",500,345);
+		words["text"] = "200 Million";
+    words["x"] = 500;
+    words["y"] = 345;
 	}
-	
+
 	var check = collidePointCircle(mouseX,mouseY, 751, 260, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
 	  noStroke;
-		text("300 Million",730,245);
+		words["text"] = "300 Million";
+    words["x"] = 730;
+    words["y"] = 245;
 	}
-	
+
 		var check = collidePointCircle(mouseX,mouseY, 889, 160, 10, 10); //see if the mouse is in the rect
 
 	if(check){ //change color!
 	  noStroke;
-		text("400 Million",860,150);
+		words["text"] = "400 Million";
+    words["x"] = 860;
+    words["y"] = 150;
 	}
-	
+
+  text(words["text"], words["x"], words["y"])
+
 	var check = collidePointRect(mouseX,mouseY, xPosition, yPosition+datayear, rectWidth, -datayear); //see if the mouse is in the rect
 
 	if(check){
 	  noStroke;
 		text(data[i],500,100);
 	}
-	
+
     if (0<= i && i <=3){
       fill(i*100,50,0);
       noStroke();
@@ -95,7 +117,7 @@ for (var i=0; i <= 16; i++){
       stroke(51);
       ellipse(150*(i-16)+3, -data[i]+600, 15, 15);
     }
-    
+
 noFill();
 stroke(51);
 ellipse(153, 470, 10, 10);
@@ -105,7 +127,7 @@ ellipse(337, 410, 10, 10);
 ellipse(517, 360, 10, 10);
 ellipse(751, 260, 10, 10);
 ellipse(889, 160, 10, 10);
-  
+
 }
 
 stroke(0);
@@ -148,14 +170,10 @@ noStroke();
 text("Instagram Population", 25, 155);
 
 
-	
+
 }
 
 
 
 var instapop = [90, 100, 130, 150, 200, 300, 400];
 var instatime= [1, 2, 6, 9, 15, 24, 33];
-
-
-
-
