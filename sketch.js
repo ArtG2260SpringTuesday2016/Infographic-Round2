7680a0969cb2685bd340f1264b6f168b27eec402
Francisco Dias <fdias@hubspot.com>
2016-04-05 21:04:12 -0400

    fixed some more things

--- sketch.js
+++ sketch.js
@@ -1,8 +1,23 @@
-var dataValueText = "artist";
-
 var obj = {};
 for (var i = 0; i < thehits.length; i++) {
    obj[thehits[i]] = (obj[thehits[i]] || 0) + 1;
+}
+
+var visTotals = function (obj) {
+  var j = 0;
+  var rectWidth = 3;
+  var barHeight;
+  var spacing;
+
+  for (var prop in obj) {
+   fill ("#bada55")
+   strokeWeight(0)
+   barHeight = obj[prop];
+   rectHeight = barHeight * -10;
+   spacing = j * (1 + rectWidth);
+   rect(spacing, 500, rectWidth, rectHeight);
+   j++;
+ }
 }
 
 function setup() {
@@ -12,27 +27,12 @@ function setup() {
   text("All The Hits" ,30,30)
   textSize(12)
   textFont("Avenir");
-  text("Which artist had the most Billboard Weekly hits from 1970-2016?", 30, 60)
-
- var visTotals = function (obj) {
-   var j = 0;
- for (var prop in obj) {
-      fill ("#bada55")
-      strokeWeight(0)
-      var barHeight = obj[prop];
-      var rectWidth = 3;
-      rectHeight = barHeight * -10;
-      var spacing = j * (1 + rectWidth);
-      rect(spacing, 500, rectWidth, rectHeight);
-      j++;
-    }
-
-  }
-  visTotals(obj);
+  text("Which artist had the most Billboard Weekly hits from 1970-2016?", 30, 60);
 }
 
 function draw() {
+  fill(333)
   textSize(22)
   textFont("Avenir");
-  text(dataValueText, 30, 100);
+  visTotals(obj);
 }
