8dc7dce176305dd2c31022b01e604d1d715b7044
zongyuanyue <yue.zo@husky.neu.edu>
2016-04-05 20:30:25 -0400

    better info graph

--- sketch.js
+++ sketch.js
@@ -21,29 +21,88 @@ var myData = [
     
   ];
 
+var title = function(){
+  textSize(50)
+  text("?who is taller?",50,50)
+  fill(100)
+}
+var dad = function(){
+  textSize(50)
+  text("dad",50,500)
+  fill(80)
+}
+var mom = function(){
+  textSize(50)
+  text("mom",150,500)
+  fill(100)
+}
+var me = function(){
+  textSize(50)
+  text("me",270,500)
+  fill(120)
+}
 
+var fifty = function(){
+  textSize(30)
+  text("50",520,420)
+  fill(120)
+}
 
+var hundred = function(){
+  textSize(30)
+  text("50",520,370)
+  fill(120)
+}
+
+var hundredfifty = function(){
+  textSize(30)
+  text("150",520,320)
+  fill(120)
+}
+ 
+ var legend = function(){
+  textSize(20)
+  text("height in cm",300,80)
+  fill(120) 
+  
+}
 function setup() {
   createCanvas(1000, 500)
   noStroke()
   fill("orange")
-   
+
+  
+  
+
+  
   var visTotals = function (someData) {
     for (var i = 0; i < someData.length; i++){
-       var pointX = i * 50;
+       var pointX = i * 100;
        var rectHeight = barHeight * 10;
        var barWidth = 10;
-       var spacing = 5 + barWidth;
+       var spacing = 450 + barWidth;
       var barHeight = someData[i].heights / 10;
-      rect(pointX, spacing, barWidth, rectHeight)
+      rect(pointX, spacing, barWidth, -rectHeight)
     }
    
+
 
    
   }
   visTotals(myData);
 }
 
-function draw() {
-
+var draw = function(){
+  
+rect(20,410,500,5)
+rect(20,360,500,5)
+rect(20,310,500,5)
+title()
+dad()
+mom()
+me()
+legend()
+fifty()
+hundred()
+hundredfifty()
 }
